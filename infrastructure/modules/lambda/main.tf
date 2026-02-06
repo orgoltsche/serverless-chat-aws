920373302b53d98d26a065bf784dfc17a8f29b02
@oliver-rene.goltsche <oliver-rene.goltsche@actindo.com>
2026-02-06 17:30:53 +0100

    Format Terraform

--- infrastructure/modules/lambda/main.tf
+++ infrastructure/modules/lambda/main.tf
@@ -75,9 +75,9 @@ resource "aws_security_group" "lambda" {
   vpc_id      = var.vpc_id
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
